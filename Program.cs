886f55bd18b7a5340a9ecab3315ecdc14848dbe4
AlejoDEV <danijandro03@outlook.com>
2021-06-16 20:38:13 -0500

    Clase 17

--- Program.cs
+++ Program.cs
@@ -12,14 +12,16 @@ namespace Etapa1
 
             //declarar arreglo del tipo Course con 3 posiciones
 
-            var arCourse = new Course [3];
+            var arCourse = new Course[3];
 
             //Creo y asigno FirstCourse a posición 0
-            arCourse[0] = new Course(){
+            arCourse[0] = new Course()
+            {
                 Name = "FirstCourse"
             };
             //Creo SecondCourse
-            var curso1 = new Course(){
+            var curso1 = new Course()
+            {
                 Name = "SecondCourse"
             };
             //Asigno SecondCourse a la posición 1
@@ -29,31 +31,63 @@ namespace Etapa1
             {
                 Name = "ThirdCourse"
             };
-                    
+            //Imprimir en consola:
             Console.WriteLine(School);
-            System.Console.WriteLine("============");
-            PrintCourse(arCourse);
+            System.Console.WriteLine("==============================");
+            PrintCourseWhile(arCourse);
+            System.Console.WriteLine("==============================");
+            PrintCourseDoWhile (arCourse);
+            System.Console.WriteLine("==============================");
+            PrintCourseFor(arCourse);
+            System.Console.WriteLine("==============================");
+            PrintCourseForEach(arCourse);
 
-        // {
-        //     
-        //     //declaro objeto con segundo constructor
-        //     var School1 = new School("Giralda", 2004, SchoolTypes.Secundaria, 
-        //                             country: "Colombia", city: "Medelin");
-        //     School.Country = "Colombia";
-        //     School.City = "Bogota";
-        //     School.SchoolType = SchoolTypes.Primaria;
-        //     Console.WriteLine(School);
-        // }
+            // {
+            //     
+            //     //declaro objeto con segundo constructor
+            //     var School1 = new School("Giralda", 2004, SchoolTypes.Secundaria, 
+            //                             country: "Colombia", city: "Medelin");
+            //     School.Country = "Colombia";
+            //     School.City = "Bogota";
+            //     School.SchoolType = SchoolTypes.Primaria;
+            //     Console.WriteLine(School);
+            // }
 
         }
 
-        private static void PrintCourse(Course[] arCourse)
+        private static void PrintCourseWhile(Course[] arCourse)
         {
             int count = 0;
             while (count < arCourse.Length)
             {
                 Console.WriteLine($"Nombre {arCourse[count].Name}, id {arCourse[count].ID}");
                 count += 1;
+            }
+        }
+        private static void PrintCourseDoWhile(Course[] arCourse)
+        {
+            int count = 0;
+            do
+            {
+                Console.WriteLine($"Nombre {arCourse[count].Name}, id {arCourse[count].ID}");
+                count++;
+            }
+            //se puede declarar contador++ (aumenta despues de comparar) 
+            //o ++contador (lo aumenta antes de comparar)
+            while (count < arCourse.Length);
+        }
+        private static void PrintCourseFor(Course[] arCourse)
+        {
+            for (int i = 0; i < arCourse.Length; i++)
+            {
+                Console.WriteLine($"Nombre {arCourse[i].Name}, id {arCourse[i].ID}");
+            }
+        }
+        private static void PrintCourseForEach(Course[] arCourse)
+        {
+            foreach (var Course in arCourse)
+            {
+                Console.WriteLine($"Nombre {Course.Name}, id {Course.ID}");
             }
         }
     }
